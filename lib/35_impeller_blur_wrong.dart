d8e7fb4add3dfd372342b31064124a41aaa5083d
Rydmike <39990307+rydmike@users.noreply.github.com>
2024-02-19 23:57:28 +0200

    Laavu test

--- lib/35_impeller_blur_wrong.dart
+++ lib/35_impeller_blur_wrong.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Impeller Blur Issue',
       theme: ThemeData(
         appBarTheme: const AppBarTheme(elevation: 0),
@@ -28,8 +29,10 @@ class MyApp extends StatelessWidget {
               clipBehavior: Clip.hardEdge,
               // overflow: Overflow.clip,
               children: <Widget>[
-                _FrostedGlass(),
+                _AppTitle(),
                 _AnimatedBackground(),
+                _FrostedGlass(),
+                _BottomSegment(),
               ],
             ),
           );
@@ -39,8 +42,47 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Put a "frosted glass" effect over half of the sun and the upside down house
-// to make them look like they are reflected in water
+class _AppTitle extends StatelessWidget {
+  const _AppTitle();
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final bool isLight = theme.brightness == Brightness.light;
+    final Color fontColor = theme.colorScheme.onSurfaceVariant;
+    final Color backgroundColor = isLight ? Colors.white : Colors.black;
+
+    return ColoredBox(
+      color: backgroundColor,
+      child: Align(
+        child: Column(
+          children: <Widget>[
+            const Spacer(),
+            // A Welcome to TALO text, laid out in a column
+            Text(
+              'Welcome to',
+              textAlign: TextAlign.center,
+              style: theme.textTheme.displaySmall,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'DEMO',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: fontColor,
+                fontSize: 45,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            const Spacer(flex: 5),
+          ],
+        ),
+        // ),
+      ),
+    );
+  }
+}
+
 class _FrostedGlass extends StatelessWidget {
   const _FrostedGlass();
 
@@ -56,7 +98,7 @@ class _FrostedGlass extends StatelessWidget {
             width: double.infinity,
             decoration: BoxDecoration(
               color:
-                  Theme.of(context).colorScheme.surfaceVariant.withOpacity(0.5),
+                  Theme.of(context).colorScheme.surfaceVariant.withOpacity(0.4),
             ),
           ),
         ),
@@ -73,10 +115,98 @@ class _AnimatedBackground extends StatelessWidget {
     return Align(
       alignment: Alignment.bottomCenter,
       child: Padding(
-          padding: const EdgeInsets.only(bottom: 185.6),
-          child: CircularProgressIndicator(
-            color: Colors.red[800],
+          padding: const EdgeInsets.only(bottom: 120),
+          child: ClipRect(
+            clipBehavior: Clip.hardEdge,
+            // Custom clipper used because ClipRect does not show correct
+            // behavior when clipping rectangles with clipBehavior
+            // Clip.hardEdge or even antialias.
+            // See issue: https://github.com/flutter/flutter/issues/58547
+            clipper: TightClipper(MediaQuery.of(context).devicePixelRatio,
+                tightFactor: 2.5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                LinearProgressIndicator(
+                  color: Colors.red[800],
+                  backgroundColor: Colors.transparent,
+                  minHeight: 20,
+                ),
+                const SizedBox(height: 20),
+                ClipRect(
+                  clipBehavior: Clip.hardEdge,
+                  clipper: TightClipper(MediaQuery.of(context).devicePixelRatio,
+                      tightFactor: 2.5),
+                  child: SizedBox(
+                    height: 150,
+                    width: 150,
+                    child: CircularProgressIndicator(
+                      color: Colors.red[800],
+                      strokeWidth: 15,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                LinearProgressIndicator(
+                  color: Colors.red[800],
+                  backgroundColor: Colors.transparent,
+                  minHeight: 20,
+                ),
+              ],
+            ),
           )),
     );
   }
+}
+
+class _BottomSegment extends StatelessWidget {
+  const _BottomSegment();
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle bottomText =
+        Theme.of(context).textTheme.displaySmall!.copyWith(fontSize: 22);
+
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: SizedBox(
+        height: 100,
+        child: Column(
+          children: <Widget>[
+            Text('Text on', textAlign: TextAlign.center, style: bottomText),
+            const SizedBox(height: 10),
+            Text('top of blur', textAlign: TextAlign.center, style: bottomText),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/// A custom clipper used because ClipRect does not show correct behavior when
+/// clipping rectangles with clipBehavior Clip.hardEdge or even antialias.
+///
+/// See issue: https://github.com/flutter/flutter/issues/58547
+class TightClipper extends CustomClipper<Rect> {
+  TightClipper(this.devicePixelRatio, {this.tightFactor = 1});
+
+  @override
+  Rect getClip(Size size) {
+    final double padding = 1 / devicePixelRatio * tightFactor;
+    return Rect.fromLTRB(
+      padding,
+      padding,
+      size.width - padding,
+      size.height - padding,
+    );
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Rect> oldClipper) {
+    return true;
+  }
+
+  final double devicePixelRatio;
+  final double tightFactor;
 }
---
+++ lib/image_blur.dart
@@ -0,0 +1,92 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class ThemeImage extends StatefulWidget {
+  const ThemeImage({
+    super.key,
+  });
+
+  @override
+  State<ThemeImage> createState() => _ThemeImageState();
+}
+
+class _ThemeImageState extends State<ThemeImage> {
+  bool soldout = false;
+
+  static const double _shapeRadius = 50.0;
+  static const double _imagePadding = 40.0;
+  static const double _imageHeight = 500.0;
+
+  @override
+  Widget build(BuildContext context) {
+    final double opacity = soldout ? 0.5 : 1.0;
+    TextStyle style = TextStyle(color: Colors.white.withOpacity(opacity));
+    return Column(
+      children: [
+        SwitchListTile(
+          title: const Text('Sold out'),
+          value: soldout,
+          onChanged: (bool value) {
+            setState(() {
+              soldout = value;
+            });
+          },
+        ),
+        SizedBox(
+          height: _imageHeight,
+          child: Padding(
+            padding: const EdgeInsets.all(_imagePadding),
+            child: Stack(
+              children: [
+                Material(
+                  color: Colors.transparent,
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(_shapeRadius),
+                  ),
+                  child: Ink.image(
+                    image: const AssetImage('assets/images/AleLaavu.jpeg'),
+                    colorFilter: soldout
+                        ? const ColorFilter.matrix(<double>[
+                            0.2126, 0.7152, 0.0722, 0, 0, // Greyscale filter
+                            0.2126, 0.7152, 0.0722, 0, 0, // Greyscale filter
+                            0.2126, 0.7152, 0.0722, 0, 0, // Greyscale filter
+                            0, 0, 0, 1, 0, // Greyscale filter
+                          ])
+                        : null,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(_shapeRadius),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+                      child: Container(
+                          height: _shapeRadius * 2,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.3),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text('Overview', style: style),
+                              Text('Rooms', style: style),
+                              Text('Promotion', style: style),
+                              Text('Amenities', style: style),
+                            ],
+                          )),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
